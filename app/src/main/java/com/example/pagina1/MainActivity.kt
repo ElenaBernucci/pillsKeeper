25961c208e4a010a02dbdb7618c36dabdd5fbf25
elena <elena.bernucci00@gmail.com>
2021-03-24 11:32:55 +0100

    il bottone funziona

--- app/src/main/java/com/example/pagina1/MainActivity.kt
+++ app/src/main/java/com/example/pagina1/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.pagina1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+
+    fun startMenuIniziale(v: View) {
+        val intent = Intent(this@MainActivity, menu_iniziale::class.java)
+        startActivity(intent)
+    }
+
 }
