13af7cc346e006bcc81d2a95b388d7ec9b6759b6
luca <lucaclementi1d@gmail.com>
2021-04-30 11:18:39 +0200

    Navgraph

--- app/src/main/java/com/example/PillsKeeper/MenuIniziale.kt
+++ app/src/main/java/com/example/PillsKeeper/MenuIniziale.kt
@@ -1,13 +1,7 @@
 package com.example.PillsKeeper
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_menu_iniziale.*
-import kotlinx.android.synthetic.main.fragment_iniziale.*
-import kotlinx.android.synthetic.main.activity_menu_iniziale.btnPlus as btnPlus1
 
 class MenuIniziale : AppCompatActivity() {
 
@@ -15,38 +9,7 @@ class MenuIniziale : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_iniziale)
 
-        val firstFragment=FirstFragment()
 
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.Fragment,firstFragment)
-            commit()
-        }
-
-    }
-
-    fun startFunzioni(v: View) {
-        val intent = Intent(this@MenuIniziale, funzioni::class.java)
-        startActivity(intent)
-    }
-
-    fun startReminder(v: View) {
-        val intent = Intent(this@MenuIniziale, funzioni::class.java)
-        startActivity(intent)
-    }
-
-    fun startFarmacia(v: View) {
-        val intent = Intent(this@MenuIniziale, funzioni::class.java)
-        startActivity(intent)
-    }
-
-    fun startDottore(v: View) {
-        val intent = Intent(this@MenuIniziale, funzioni::class.java)
-        startActivity(intent)
-    }
-
-    fun startContatti(v: View) {
-        val intent = Intent(this@MenuIniziale, funzioni::class.java)
-        startActivity(intent)
     }
 
 }
--- app/src/main/java/com/example/PillsKeeper/fragmentIniziale.kt
+++ app/src/main/java/com/example/PillsKeeper/fragmentIniziale.kt
@@ -1,9 +1,37 @@
 package com.example.PillsKeeper
 
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 
-class FirstFragment : Fragment(R.layout.fragment_iniziale) {
-    
+class FirstFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navc:NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_iniziale, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navc= Navigation.findNavController(view)
+        view.findViewById<ImageButton>(R.id.imageButtonReminder).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        navc?.navigate(R.id.iniziale_to_iniziale_accesso)
+    }
+
 }
